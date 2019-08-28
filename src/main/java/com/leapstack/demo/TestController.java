6cb6123136642b037b41d22693165fe9216ade0b
虞明 <yuming@yumingdeMacBook-Pro.local>
2019-08-28 12:01:57 +0800

    第一次提交

---
+++ src/main/java/com/leapstack/demo/TestController.java
@@ -0,0 +1,14 @@
+package com.leapstack.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("say")
+    public String toStsring() {
+
+        return "第一个spring-boot项目";
+    }
+}
